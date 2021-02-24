841e8b8851c98556f5e669e5e5f4c715cef80b35
Carlos Bueno <cabueno@homeaway.com>
2021-02-24 15:58:30 -0500

    afjustments

--- WEB/js/script.js
+++ WEB/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
+
     var response = null;
+    
     var getMaxDate = function(){
         let date = new Date();
         date.setFullYear(date.getFullYear() -18);
         return date.toISOString().split('T')[0]
-        // return date
     }
 
     var ageChecker = function(){
@@ -29,18 +30,12 @@ $(document).ready(function () {
     }
 
     // datepicker max date => today
-    // $('#dateOfBirth')[0].max = getMaxDate().toISOString().split('T')[0]
     $('#dateOfBirth')[0].max = getMaxDate()
     // get value button is diabled unless all fields have data and checks are passed
     $('#getValue')[0].disabled=true;
 
 
     // gets age from DOB 
-    
-
-    
-
-
     $('#dateOfBirth').change(function(){
         ageChecker();
         mainCheck();
@@ -57,37 +52,35 @@ $(document).ready(function () {
     $('#distribution').change(function(){
         mainCheck();
         if (response !== null){
-            $('#premiun')[0].value = $('#distribution')[0].value*response.premium;
+            $('#premiun')[0].value = $('#distribution')[0].value * response.premium;
         }
     })
 
-    
-
-
-
     $('#getValue').click(function(){
         ageChecker()
+        url = 'https://127.0.0.1:5002/premium'
         let payload = {
             DateOfBirth : $('#dateOfBirth')[0].value,
             Age : $('#age')[0].value,
             State : $('#state')[0].value
         }
+        console.log(payload)
 
-        // $.post("https://localhost:5001/premium", payload,
-        //     function (data, textStatus, jqXHR) {
-        //         response = data.premium;
-        //         $('#annual')[0].text = 12*response;
-        //         $('#monthly')[0].text = response;
-                
-        //     },
-        // );
-        response = {
-            "premium": 90
+        $.post( url, function( data ) {
+            response = data.premium;
+        });
+
+
+
+        if (response !==null){
+            $('#annual')[0].value   = response.premium * 12;
+            $('#monthly')[0].value  = response.premium;
+            $('#premiun')[0].value  = response.premium * $('#distribution')[0].value;
         }
-        $('#annual')[0].value = 12*response.premium;
-        $('#monthly')[0].value = response.premium;
-        $('#premiun')[0].value = response.premium*$('#distribution')[0].value;
     })
 
     
 });
+
+
+
